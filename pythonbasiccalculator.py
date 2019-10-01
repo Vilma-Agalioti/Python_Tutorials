f97a5a581e613890b593c667a0ac4062565ac1b5
“[firstname <viveksilimkhan@gmail.com>
2019-10-02 03:04:28 +0530

    Added pythonbasiccalculator.py

---
+++ pythonbasiccalculator.py
@@ -0,0 +1,11 @@
+import math 
+num1=input("Enter the number:")
+num2=input("Enter the number:")
+num3=input("Enter the number:")
+num4=input("Enter the number:")
+#int is used to convert the number saved into integer
+result1=float(num1)+float(num2)
+result2=int(num1)-int(num2)
+result3=int(num1)*int(num2)
+result4=int(num1)/int(num2)
+print(result1+result2+result3+result4)
