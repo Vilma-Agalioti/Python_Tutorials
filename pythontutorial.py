7f6e85514f4b47f0f991c8b94e1f186df4d8a7a6
“[firstname <viveksilimkhan@gmail.com>
2019-10-02 03:02:02 +0530

    Added a pythontutorial.py

---
+++ pythontutorial.py
@@ -0,0 +1,23 @@
+from math import *
+string="I am Quark"
+print(string)
+print(string[2])
+print(string.index("Quark"))
+print(string.replace("Quark","Quarkcharge"))
+print(3*(4+5))
+num=10
+print(10%3)
+print(num)
+print(str(num) + "is ten")
+print(abs(num-10*num))
+print(pow(3,2))
+print(max(3,2))
+print(min(4,3))
+print(round(5,4))
+print(ceil(5) + floor(5))
+print("Enter")
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Hello " + name + "!" + "you are "+ age)
+
+
